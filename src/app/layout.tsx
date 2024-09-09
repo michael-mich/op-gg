693ff167c4eaafcbd63de70909e36496ba63cbf6
Michael <michael.github333@gmail.com>
2024-09-09 12:50:12 +0200

    Refactor: Update import file path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Provider from './_lib/providers/provider';
 import ReactQueryProvider from './_lib/providers/ReactQueryProvider';
 import StoreProvider from './_lib/providers/StoreProvider';
 import NextUiProvider from './_lib/providers/NextUiProvider';
-import Navigation from './_components/navigation/navigation';
+import Navigation from './_components/navigation/Navigation';
 import Footer from './_components/footer/footer';
 import './globals.css';
 
