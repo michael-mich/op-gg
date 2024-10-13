0c2248f5659f0575b6267a7f35d523a0c228cbd1
Michael <michael.github333@gmail.com>
2024-10-13 13:00:14 +0200

    Refactor: Remove QueueTypes and restore SummonerChampionMastery

--- src/app/summoners/[region]/[summonerData]/(summaryPage)/page.tsx
+++ src/app/summoners/[region]/[summonerData]/(summaryPage)/page.tsx
@@ -1,4 +1,3 @@
-import QueueTypes from './QueueTypes';
 import SummonerRank from './_summonerRank/SummonerRank';
 import SummonerChampionsMastery from '@/app/_components/summonerChampionsMastery/SummonerChampionsMastery';
 import { QueueType } from '@/app/_enums/enums';
@@ -7,7 +6,6 @@ import RecentGames from './_recentGames/RecentGames';
 const Page = () => {
   return (
     <>
-      <QueueTypes />
       <div className='flex gap-2 mt-2'>
         <div className='flex flex-col gap-2 w-[332px] min-w-[332px]'>
           <SummonerRank
@@ -18,7 +16,7 @@ const Page = () => {
             queueType={QueueType.RankedFlex}
             smallDataStyle
           />
-          {/* <SummonerChampionsMastery /> */}
+          <SummonerChampionsMastery />
         </div>
         <RecentGames />
       </div>
