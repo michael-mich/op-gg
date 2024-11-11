a445b1df52cedbf9559d2cced5aa0a0de4969d63
Michael <michael.github333@gmail.com>
2024-11-11 18:13:50 +0100

    Refactor: Add missing queue types

--- src/app/summoners/[region]/[summonerData]/(summaryPage)/_summonerMatchOverview/matchHistory/utils/utils.ts
+++ src/app/summoners/[region]/[summonerData]/(summaryPage)/_summonerMatchOverview/matchHistory/utils/utils.ts
@@ -1,17 +1,19 @@
 import type { TSummonerDetailedMatchHistory } from '@/app/_types/apiTypes/customApiTypes';
 
-export const checkSummonerKills = (currentSummoner: TSummonerDetailedMatchHistory) => {
-  if (currentSummoner.pentaKills > 0) {
-    return 'Penta';
-  }
-  else if (currentSummoner.quadraKills > 0) {
-    return 'Quadra';
-  }
-  else if (currentSummoner.tripleKills > 0) {
-    return 'Tripple';
-  }
-  else if (currentSummoner.doubleKills > 0) {
-    return 'Double';
+export const checkSummonerKills = (currentSummoner: TSummonerDetailedMatchHistory | undefined) => {
+  if (currentSummoner) {
+    if (currentSummoner.pentaKills > 0) {
+      return 'Penta';
+    }
+    else if (currentSummoner.quadraKills > 0) {
+      return 'Quadra';
+    }
+    else if (currentSummoner.tripleKills > 0) {
+      return 'Tripple';
+    }
+    else if (currentSummoner.doubleKills > 0) {
+      return 'Double';
+    }
   }
 }
 
@@ -30,9 +32,11 @@ export const checkQueueType = (queueId: number): string => {
     case 1200: return 'Nexus Blitz';
     case 1300: return 'Blitz';
     case 1400: return 'Ultimate Spellbook';
+    case 1900: return 'URF';
     case 2000: return 'Tutorial 1';
     case 2010: return 'Tutorial 2';
     case 2020: return 'Tutorial 3';
+    case 4501: return 'ARAM Clash';
     default: return 'Unknown Queue Type';
   }
 }
