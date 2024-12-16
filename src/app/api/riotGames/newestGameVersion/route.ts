02c58af894f31c63e9ef23a6c4509c7ff043d98c
Michael <michael.github333@gmail.com>
2024-12-16 09:44:17 +0100

    Fix(routeHandler): Convert to dynamic route for retrieving the lastest game version

--- src/app/api/riotGames/newestGameVersion/route.ts
+++ src/app/api/riotGames/newestGameVersion/route.ts
@@ -1,6 +1,6 @@
 import { fetchApi } from '@/app/_utils/fetchApi';
 
 export const GET = async () => {
-  const gameVersions = await fetchApi<Array<string>>('https://ddragon.leagueoflegends.com/api/versions.json');
+  const gameVersions = await fetchApi<Array<string>>(`https://ddragon.leagueoflegends.com/api/versions.json?timestamp=${Date.now()}`);
   return Response.json(gameVersions?.[0]);
 }
