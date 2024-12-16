9371704adb0d8dacb2d64848f32288d699818062
Michael <michael.github333@gmail.com>
2024-12-16 10:45:46 +0100

    Refactor(useGameVersionQuery): Add 'gcTime' to query

--- src/app/_hooks/queries/useGameVersionQuery.ts
+++ src/app/_hooks/queries/useGameVersionQuery.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchApi } from '@/app/_utils/fetchApi';
 import { riotGamesRoutes } from '@/app/_constants/endpoints';
+import { TEN_MINUTES } from '@/app/_constants/timeUnits';
 
 const useGameVersionQuery = () => {
   return useQuery({
     queryKey: ['newestGameVersion'],
     queryFn: () => fetchApi<string>(riotGamesRoutes.newestGameVersion()),
-    staleTime: 24 * 60 * 60 * 1000 // 1 day
+    staleTime: 24 * 60 * 60 * 1000, // 1 day
+    gcTime: TEN_MINUTES
   });
 }
 
