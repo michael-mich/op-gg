c76df55ff0f2dfa24201b2e5b660cdd0edd8b65e
Michael <michael.github333@gmail.com>
2024-12-22 17:03:20 +0100

    Chore: Rename Footer to follow capitalization convention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import ReactQueryProvider from './_lib/providers/ReactQueryProvider';
 import StoreProvider from './_lib/providers/StoreProvider';
 import NextUiProvider from './_lib/providers/NextUiProvider';
 import Navigation from './_components/navigation/Navigation';
-import Footer from './_components/footer/footer';
+import Footer from './_components/footer/Footer';
 import './globals.css';
 
 const roboto = Roboto({
