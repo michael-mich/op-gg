7876c3b824c180cd198cf6a09f55c959823e2769
Michael <michael.github333@gmail.com>
2025-01-10 12:07:39 +0100

    Fix: Implement revalidation in route handlers to keep data up to date

--- src/app/api/riotGames/newestGameVersion/route.ts
+++ src/app/api/riotGames/newestGameVersion/route.ts
@@ -1,6 +1,9 @@
 import { fetchApi } from '@/app/_utils/fetchApi';
+import { ONE_DAY } from '@/app/_constants/timeUnits';
+
+export const revalidate = ONE_DAY;
 
 export const GET = async () => {
-  const gameVersions = await fetchApi<Array<string>>(`https://ddragon.leagueoflegends.com/api/versions.json?timestamp=${Date.now()}`);
+  const gameVersions = await fetchApi<Array<string>>(`https://ddragon.leagueoflegends.com/api/versions.json`);
   return Response.json(gameVersions?.[0]);
 }
--- src/app/api/riotGames/runes/route.ts
+++ src/app/api/riotGames/runes/route.ts
@@ -1,6 +1,9 @@
 import { fetchApi } from '@/app/_utils/fetchApi';
 import { riotGamesRoutes } from '@/app/_constants/endpoints';
+import { ONE_DAY } from '@/app/_constants/timeUnits';
 import type { TRune } from '@/app/_types/apiTypes/apiTypes';
+
+export const revalidate = ONE_DAY;
 
 export const GET = async () => {
   const newestGameVersion = await fetchApi(riotGamesRoutes.newestGameVersion());
